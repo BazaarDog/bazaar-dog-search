feb714fc2bdfe2fbe57d37ca6a257604609d13db
BazaarDog <ar>
2019-01-22 08:26:36 -0500

    refactor form widgets

--- ob/api/widgets.py
+++ ob/api/widgets.py
@@ -2,29 +2,13 @@ from django import forms
 from django.utils.translation import ugettext as _
 
 
-class TruthyWidget(forms.Select):
-    """Convert true/false values into the internal Python True/False.
-    This can be used for AJAX queries that pass true/false from JavaScript's
-    internal types through.
-    """
+class TweekedWidget(forms.Select):
 
     def __init__(self, attrs=None):
         choices = (('', _('Unknown')),
                    ('true', _('Yes')),
                    ('false', _('No')))
         super().__init__(attrs, choices)
-
-    def render(self, name, value, attrs=None, renderer=None):
-        try:
-            value = {
-                True: 'true',
-                '': 'false',
-                '1': 'true',
-                '': 'false'
-            }[value]
-        except KeyError:
-            value = ''
-        return super().render(name, value, attrs, renderer)
 
     def value_from_datadict(self, data, files, name):
         value = data.get(name, None)
@@ -41,17 +25,21 @@ class TruthyWidget(forms.Select):
         }.get(value, None)
 
 
-class FalsyWidget(forms.Select):
-    """Convert true/false values into the internal Python True/False.
-    This can be used for AJAX queries that pass true/false from JavaScript's
-    internal types through.
-    """
+class TruthyWidget(TweekedWidget):
 
-    def __init__(self, attrs=None):
-        choices = (('', _('Unknown')),
-                   ('true', _('Yes')),
-                   ('false', _('No')))
-        super().__init__(attrs, choices)
+    def render(self, name, value, attrs=None, renderer=None):
+        try:
+            value = {
+                True: 'true',
+                '': 'false',
+                '1': 'true',
+                '': 'false'
+            }[value]
+        except KeyError:
+            value = ''
+        return super().render(name, value, attrs, renderer)
+
+class FalsyWidget(TweekedWidget):
 
     def render(self, name, value, attrs=None, renderer=None):
         try:
@@ -66,16 +54,4 @@ class FalsyWidget(forms.Select):
             value = ''
         return super().render(name, value, attrs, renderer)
 
-    def value_from_datadict(self, data, files, name):
-        value = data.get(name, None)
-        if isinstance(value, str):
-            value = value.lower()
 
-        return {
-            '1': True,
-            '0': '',
-            'true': True,
-            'false': '',
-            True: True,
-            False: '',
-        }.get(value, None)
