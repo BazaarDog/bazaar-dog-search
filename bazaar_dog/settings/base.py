51f14e9d22ce85cf804648f2b46d40af2ba47857
BazaarDog <ar>
2019-04-15 04:51:17 -0400

    fix system checks

--- bazaar_dog/settings/base.py
+++ bazaar_dog/settings/base.py
@@ -74,8 +74,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'admin.bazaar.dog', ]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
