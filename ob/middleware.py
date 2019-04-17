47441a6bd57883c67fa921a8fe02c482bbfde613
BazaarDog <ar>
2019-04-17 04:37:26 -0400

    Clean up middleware

--- ob/middleware.py
+++ ob/middleware.py
@@ -6,19 +6,11 @@ from django.conf import settings
 
 class HumanizeMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if 'HTTP_ORIGIN' in request.META:
-            if 'bazaar.dog' in request.META['HTTP_ORIGIN']:
-                pass
-            if 'localhost' in request.META['HTTP_ORIGIN']:
-                pass
-
-            # this redirects traffic not originating from ob to a human readable website
-            elif not settings.DEV and not settings.DEBUG:
-                try:
-                    if "OpenBazaar" not in request.META['HTTP_USER_AGENT']:
-                        return redirect('https://www.bazaar.dog/')
-                except KeyError:
-                    return redirect('https://www.bazaar.dog/')
+        # this redirects traffic not originating from ob to
+        # a human readable website
+        if not settings.DEV and not settings.DEBUG:
+            if "OpenBazaar" not in request.META.get('HTTP_USER_AGENT'):
+                return redirect('https://www.bazaar.dog/')
 
 
 class ForceCORS(MiddlewareMixin):
