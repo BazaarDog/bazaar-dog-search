5d72b262ca8edc32a97fa8d49711950568dcb76c
BazaarDog <ar>
2019-05-04 06:05:37 -0400

    remove duplicate sample settings

--- bazaar_dog/settings/test.py
+++ bazaar_dog/settings/test.py
@@ -1,19 +1,6 @@
-import os
 from .base import *
 
-DEBUG = False
-
-SITE_NAME = 'Bazaar Dog'
-SITE_URL = 'https://bazaar.dog'
-
-
-ALLOWED_HOSTS = ['bazaar.dog']
-
-API_GATEWAY_HOST = os.getenv('API_GATEWAY_HOST')
-
-if API_GATEWAY_HOST:
-    ALLOWED_HOSTS += [API_GATEWAY_HOST]
-
+# This is to speed up tests (possibly), not that passwords are ever used
 PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.MD5PasswordHasher',
 ]
